a6d2fda7fe42742691c8d3eb88d31cbeb335bed9
opulencedeveloper <victorkudos@gmail.com>
2023-11-13 21:47:36 +0100

    added-six

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,7 +4,7 @@ const Layout = (props) => {
   const { navSwitcherHandler, children, selectedTab } = props;
   return (
     <section className="relative h-screen overflow-hidden max-w-[40rem] mx-auto">
-      <main className="relative w-full h-full scrollbar-hide overflow-y-auto mt-7 pb-10">
+      <main className="relative w-full h-full scrollbar-hide overflow-y-auto mt-7 pb-28">
         {children}
       </main>
       <NavBar
