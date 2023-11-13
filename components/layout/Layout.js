23671f62cc42410b8d5eca9dc84754b598054ca3
opulencedeveloper <victorkudos@gmail.com>
2023-11-13 20:08:00 +0100

    added-five

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,16 @@
 import NavBar from "../UI/NavBar";
 
 const Layout = (props) => {
-  const {navSwitcherHandler, children, selectedTab} = props;
+  const { navSwitcherHandler, children, selectedTab } = props;
   return (
-    <section className="h-screen"><main className="relative max-w-[40rem] h-full mx-auto scrollbar-hide overflow-y-auto mt-7 pb-20">
-      {children}
-    </main>
-    <NavBar navSwitcherHandler={navSwitcherHandler} selectedTab={selectedTab}/>
+    <section className="h-screen">
+      <main className="relative max-w-[40rem] h-full mx-auto scrollbar-hide overflow-y-auto mt-7 pb-20">
+        {children}
+      </main>
+      <NavBar
+        navSwitcherHandler={navSwitcherHandler}
+        selectedTab={selectedTab}
+      />
     </section>
   );
 };
