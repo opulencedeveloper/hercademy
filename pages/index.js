bbd6cdfbb13a182ea4eb9ec8411088509664487d
opulencedeveloper <victorkudos@gmail.com>
2023-11-14 18:47:03 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import Layout from "@/components/layout/Layout";
-import ExploreTab from "@/components/tabs/ExploreTab";
 
 import HomeTab from "@/components/tabs/HomeTab";
-import WishListTab from "@/components/tabs/WishListTab";
 import { useState } from "react";
 
 export default function Home() {
-  const [selectedTab, setSelectedTab] = useState("Home");
-
-  const navSwitcherHandler = (val) => {
-    setSelectedTab(val);
-  };
   return (
     <Layout>
-      {" "}
       <HomeTab />
     </Layout>
   );
